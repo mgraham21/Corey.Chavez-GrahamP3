5b2d6e8fccbb25c6ceb623022e7c41cb85f50f1d
Mariah Chavez-Graham <58267032+mgraham21@users.noreply.github.com>
2023-03-14 14:46:27 -0600

    Added some basic functions

---
+++ Corey.Chavez-GrahamP3/Corey.Chavez-GrahamP3.cpp
@@ -0,0 +1,36 @@
+// Corey.Chavez-GrahamP3.cpp : Bobby Corey & Mariah Chavez-Graham, bcorey3@cnm.edu & mgraham21@cnm.edu
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string name;
+float earthWeight, planetWeight, planetGravity, sunDistance, travelDistance, travelSpeed;
+
+vector<string> planetNames{ "Mercury", "Venus", "Earth", "Mars", "Jupiter","Saturn", "Uranus", "Neptune" };
+vector<int> distanceSun{ 36, 67, 93, 141, 483, 886, 1782, 2793 };
+vector<float> surfaceGravity{ 0.27, 0.86, 1.00, 0.37, 2.64, 1.17, 0.92, 1.44 };
+
+void newWeight()
+{
+//calculates planet weight = earth weight * planet gravity	
+}
+
+void travelTime()
+{
+	//calculates travel time = travel distance / travel speed
+}
+
+int main()
+{
+	cout << "Bobby Corey & Mariah Chavez-Graham, "; /*Need program name and description*/
+
+	do
+	{
+
+	} while (true);
+}
+
