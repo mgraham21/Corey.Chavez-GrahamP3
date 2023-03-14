8b62d47d85e1ed09f6c0c292c9c465f9c2c9a6a5
Mariah Chavez-Graham <58267032+mgraham21@users.noreply.github.com>
2023-03-14 14:46:27 -0600

    Add project files.

---
+++ Corey.Chavez-GrahamP3/Corey.Chavez-GrahamP3.cpp
@@ -0,0 +1,21 @@
+// Corey.Chavez-GrahamP3.cpp : Bobby Corey & Mariah Chavez-Graham, bcorey3@cnm.edu & mgraham21@cnm.edu
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> planetNames{ "Mercury", "Venus", "Earth", "Mars", "Jupiter","Saturn", "Uranus", "Neptune" };
+vector<int> distanceSun{ 36, 67, 93, 141, 483, 886, 1782, 2793 };
+vector<float> surfaceGravity{ 0.27, 0.86, 1.00, 0.37, 2.64, 1.17, 0.92, 1.44 };
+
+
+
+int main()
+{
+
+   
+}
+
